8d5981ad3707bc5157d446805d44c7851ecfd919
CovertJaguar <covertjaguar@gmail.com>
2017-06-08 18:05:10 -0700

    Modify to look for Railcraft's Ore Generators instead of using config files.

--- src/main/java/cpw/mods/retro/WorldRetrogenRailcraft.java
+++ src/main/java/cpw/mods/retro/WorldRetrogenRailcraft.java
@@ -18,24 +18,12 @@
 
 package cpw.mods.retro;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.ListMultimap;
-import com.google.common.collect.Lists;
-import com.google.common.collect.MapMaker;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
+import com.google.common.collect.*;
 import com.google.common.collect.Sets.SetView;
-import net.minecraft.command.CommandBase;
-import net.minecraft.command.CommandException;
-import net.minecraft.command.ICommandSender;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
@@ -54,28 +42,25 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.registry.IForgeRegistryEntry;
 import org.apache.logging.log4j.Level;
 
-import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Random;
-import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
+import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
-@Mod(modid="simpleretrogen", name="Simple Retrogen", acceptableRemoteVersions="*", acceptedMinecraftVersions = "[1.9,1.11)")
+@Mod(modid = "railcraftretrogen", name = "Railcraft Retrogen", acceptableRemoteVersions = "*", acceptedMinecraftVersions = "[1.9,1.11)")
 @ParametersAreNonnullByDefault
-public class WorldRetrogen {
-    private List<Marker> markers = Lists.newArrayList();
+public class WorldRetrogenRailcraft {
+    private Multimap<String, String> markers = HashMultimap.create();
     private Map<String,TargetWorldWrapper> delegates;
 
     private Map<World,ListMultimap<ChunkPos,String>> pendingWork;
@@ -86,15 +71,6 @@ public class WorldRetrogen {
     private int maxPerTick;
     private Map<String,String> retros = Maps.newHashMap();
 
-    private static class Marker {
-        private final String marker;
-        private final Set<String> classes;
-
-        Marker(String marker, Set<String> classes) {
-            this.marker = marker;
-            this.classes = classes;
-        }
-    }
     @EventHandler
     public void preInit(FMLPreInitializationEvent evt)
     {
@@ -104,54 +80,6 @@ public class WorldRetrogen {
         Property property = cfg.get(Configuration.CATEGORY_GENERAL, "maxPerTick", 100);
         property.setComment("Maximum number of retrogens to run in a single tick");
         this.maxPerTick = property.getInt(100);
-        Property amProperty = cfg.get(Configuration.CATEGORY_GENERAL, "markerList", new String[0]);
-        amProperty.setComment("Active markers");
-        final List<String> activeMarkerList = Lists.newArrayList(amProperty.getStringList());
-        Set<String> categories = cfg.getCategoryNames();
-
-        if (categories.size() == 1) // only the general category - version 1 of config file
-        {
-            property = cfg.get(Configuration.CATEGORY_GENERAL, "worldGens", new String[0]);
-            String[] retros = property.getStringList();
-            property = cfg.get(Configuration.CATEGORY_GENERAL, "marker", "CPWRGMARK");
-            Marker m = new Marker(property.getString(), Sets.newHashSet(retros));
-            this.markers.add(m);
-        }
-        else
-        {
-            for (String marker : activeMarkerList) {
-                if (categories.contains(marker)) {
-                    final Property property1 = cfg.get(marker, "worldGens", new String[0]);
-                    property1.setComment("World Generator classes for marker");
-                    this.markers.add(new Marker(marker, Sets.newHashSet(property1.getStringList())));
-                    cfg.getCategory(marker).setComment("Marker definition\nYou can create as many of these as you wish\nActivate by adding to active list");
-                } else {
-                    evt.getModLog().log(Level.INFO, "Ignoring missing marker definition for active marker %s", marker);
-                }
-            }
-        }
-
-        // clean up leftovers
-        cfg.getCategory(Configuration.CATEGORY_GENERAL).remove("worldGens");
-        cfg.getCategory(Configuration.CATEGORY_GENERAL).remove("marker");
-
-        for (Marker m : markers) {
-            for (String clz : m.classes) {
-                if (retros.put(clz, m.marker) != null) {
-                    evt.getModLog().log(Level.ERROR, "Configuration error, duplicate class for multiple markers found : %s", clz);
-                }
-            }
-            if (!categories.contains(m.marker)) {
-                Property p = cfg.get(m.marker, "worldGens",new String[0]);
-                p.setComment("World Generator classes for marker");
-                p.set(m.classes.toArray(new String[0]));
-                cfg.getCategory(m.marker).setComment("Marker definition\nYou can create as many of these as you wish\nActivate by adding to active list");
-                if (!activeMarkerList.contains(m.marker)) {
-                    activeMarkerList.add(m.marker);
-                    amProperty.set(activeMarkerList.toArray(new String[0]));
-                }
-            }
-        }
 
         if (cfg.hasChanged())
         {
@@ -164,85 +92,44 @@ public class WorldRetrogen {
     }
 
     @EventHandler
-    public void serverStarting(FMLServerStartingEvent evt) {
-        evt.registerServerCommand(new CommandBase()
-        {
-            @Override
-            @Nonnull
-            public String getCommandName()
-            {
-                return "listretrogenclasstargets";
-            }
-
-            @Override
-            @Nonnull
-            public String getCommandUsage(ICommandSender sender)
-            {
-                return "List retrogens";
-            }
-
-            @Override
-            public int getRequiredPermissionLevel()
-            {
-                return 0;
-            }
-
-            @Override
-            public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
-            {
-                Set<IWorldGenerator> worldGens = ObfuscationReflectionHelper.getPrivateValue(GameRegistry.class, null, "worldGenerators");
-                List<String> targets = Lists.newArrayList();
-                for (IWorldGenerator worldGen : worldGens) {
-                    if (!(worldGen instanceof TargetWorldWrapper))
-                    {
-                        targets.add(worldGen.getClass().getName());
-                    }
-                }
-                if (targets.isEmpty()) {
-                    sender.addChatMessage(new TextComponentString("There are no retrogen target classes"));
-                } else {
-                    sender.addChatMessage(new TextComponentString(CommandBase.joinNiceStringFromCollection(targets)));
-                }
-            }
-        });
-    }
-    @EventHandler
     public void serverAboutToStart(FMLServerAboutToStartEvent evt)
     {
         this.pendingWork = new MapMaker().weakKeys().makeMap();
         this.completedWork = new MapMaker().weakKeys().makeMap();
         this.completedWorkLocks = new MapMaker().weakKeys().makeMap();
+        this.markers.clear();
 
         Set<IWorldGenerator> worldGens = ObfuscationReflectionHelper.getPrivateValue(GameRegistry.class, null, "worldGenerators");
         Map<IWorldGenerator,Integer> worldGenIdx = ObfuscationReflectionHelper.getPrivateValue(GameRegistry.class, null, "worldGeneratorIndex");
 
-        for (String retro : ImmutableSet.copyOf(retros.keySet()))
+        for (Iterator<IWorldGenerator> iterator = worldGens.iterator(); iterator.hasNext(); )
         {
-            if (!delegates.containsKey(retro))
+            IWorldGenerator wg = iterator.next();
+            if (wg.getClass().getSimpleName().equals("GeneratorRailcraftOre"))
             {
-                FMLLog.info("Substituting worldgenerator %s with delegate", retro);
-                for (Iterator<IWorldGenerator> iterator = worldGens.iterator(); iterator.hasNext();)
+                if (wg instanceof BooleanSupplier && wg instanceof Supplier && wg instanceof IForgeRegistryEntry)
                 {
-                    IWorldGenerator wg = iterator.next();
-                    if (wg.getClass().getName().equals(retro))
+
+                    boolean retrogenEnabled = ((BooleanSupplier) wg).getAsBoolean();
+                    String retrogenMarker = ((Supplier<String>) wg).get();
+                    ResourceLocation name = ((IForgeRegistryEntry) wg).getRegistryName();
+
+                    if (retrogenEnabled && !delegates.containsKey(name))
                     {
+                        FMLLog.info("Substituting worldgenerator %s with delegate", name);
                         iterator.remove();
                         TargetWorldWrapper tww = new TargetWorldWrapper();
                         tww.delegate = wg;
-                        tww.tag = retro;
+                        tww.tag = name.toString();
                         worldGens.add(tww);
                         Integer idx = worldGenIdx.remove(wg);
                         worldGenIdx.put(tww, idx);
-                        FMLLog.info("Successfully substituted %s with delegate", retro);
-                        delegates.put(retro, tww);
+                        FMLLog.info("Successfully substituted %s with delegate", name);
+                        delegates.put(name.toString(), tww);
+                        markers.put(retrogenMarker, name.toString());
+                        retros.put(name.toString(), retrogenMarker);
                         break;
                     }
-                }
-
-                if (!delegates.containsKey(retro))
-                {
-                    FMLLog.warning("WorldRetrogen was not able to locate world generator class %s, it will be skipped, found %s", retro, worldGens);
-                    retros.remove(retro);
                 }
             }
         }
@@ -334,16 +221,16 @@ public class WorldRetrogen {
         Chunk chk = chunkevt.getChunk();
         Set<String> existingGens = Sets.newHashSet();
         NBTTagCompound data = chunkevt.getData();
-        for (Marker m : markers)
+        for (String m : markers.keySet())
         {
-            NBTTagCompound marker = data.getCompoundTag(m.marker);
+            NBTTagCompound marker = data.getCompoundTag(m);
             NBTTagList tagList = marker.getTagList("list", 8);
             for (int i = 0; i < tagList.tagCount(); i++)
             {
                 existingGens.add(tagList.getStringTagAt(i));
             }
 
-            SetView<String> difference = Sets.difference(m.classes, existingGens);
+            SetView<String> difference = Sets.difference(new HashSet<>(markers.get(m)), existingGens);
             for (String retro : difference)
             {
                 if (retros.containsKey(retro))
